37fde2d2d0189d74163eb834747bb7a9b951ac48
Simon Dodsley <simon@purestorage.com>
2021-06-28 11:41:37 -0400

    Add config options for all supported features
    Change-Id: If75aab5d8a0aeb4492a8aa029494abea4a362085
    Co-authored-by: Aurelien Lourot <aurelien.lourot@canonical.com>

--- src/lib/charm/openstack/cinder_purestorage.py
+++ src/lib/charm/openstack/cinder_purestorage.py
@@ -23,12 +23,73 @@ class CinderpurestorageCharm(
         }
         service = self.config.get('volume-backend-name')
         volumedriver = drivers.get(self.config.get('protocol'))
+        image_cache = []
+        iscsi = []
+        repl = []
         driver_options = [
             ('san_ip', self.config.get('san-ip')),
             ('pure_api_token', self.config.get('pure-api-token')),
+            ('use_multipath_for_image_xfer', self.config.get('use-multipath')),
+            ('image_volume_cache_enabled',
+                self.config.get('use-image-cache')),
+            ('pure_eradicate_on_delete',
+                self.config.get('eradicate-on-delete')),
+            ('pure_automatic_max_oversubscription_ratio',
+                self.config.get('automatic-max-oversubscription')),
             ('volume_driver', volumedriver),
             ('volume_backend_name', service)]
-        return driver_options
+
+        if self.config.get('protocol') == 'iscsi':
+            if self.config.get('iscsi-cidr'):
+                iscsi.extend([('pure_iscsi_cidr',
+                             self.config.get('iscsi-cidr'))])
+            if self.config.get('use-chap'):
+                iscsi.extend([('use_chap_auth',
+                             self.config.get('use-chap'))])
+
+        if self.config.get('use-image-cache'):
+            image_cache = [
+                ('image_volume_cache_max_size_gb',
+                    self.config.get('image-volume-cache-max-size-gb', 0)),
+                ('image_volume_cache_max_count',
+                    self.config.get('image-volume-cache-max-count', 0))]
+
+        if self.config.get('use-replication'):
+            replication_device = 'backend_id:' + \
+                self.config.get('replication-target-name') + \
+                ',san_ip:' + \
+                self.config.get('replication-target-address') + \
+                ',api_token:' + \
+                self.config.get('replication-target-api-token')
+            if self.config.get('replication-type') == 'sync':
+                replication_device += ',type:sync'
+                if self.config.get('replication-sync-uniform', False):
+                    replication_device += ',uniform:true'
+            repl = [('replication_device',
+                    replication_device)]
+            if self.config.get('replica-interval'):
+                repl.extend([('pure_replica_interval_default',
+                            self.config.get('replica-interval'))])
+            if self.config.get('pure-replica-retention-short'):
+                repl.extend([('pure_replica_retention_short_term_default',
+                            self.config.get('pure-replica-retention-short'))])
+            if self.config.get('replica-retention-per-day'):
+                # required for pep8 max line length compliance
+                TEMP_VAR = 'pure_replica_retention_long_term_per_day_default'
+                repl.extend([(TEMP_VAR,
+                            self.config.get('replica-retention-per-day'))])
+            if self.config.get('replica-retention-long'):
+                repl.extend([('pure_replica_retention_long_term_default',
+                            self.config.get('replica-retention-long'))])
+            if self.config.get('replication-pgname'):
+                repl.extend([('pure_replication_pg_name',
+                            self.config.get('replication-pgname'))])
+            if self.config.get('replication-pod'):
+                repl.extend([('pure_replication_pod_name',
+                            self.config.get('replication-pod'))])
+
+        final_options = driver_options + image_cache + repl + iscsi
+        return final_options
 
 
 class CinderpurestorageCharmRocky(CinderpurestorageCharm):
